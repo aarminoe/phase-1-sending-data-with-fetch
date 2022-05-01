3e9d5dc9cb049e8afca1826e8d704b29d4b54506
Aaron Noe <aarminoe@gmail.com>
2022-05-01 15:09:19 -0500

    commit tree b836fdcb3afbac49abc0d4c31820c2e794a88500

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Add your code here
+/*
+function submitData(name, email) {
+    const configObj = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify( {
+            name: name,
+            email: email
+        })
+    }
+    return fetch('http://localhost:3000/users', configObj)
+    .then(function (response){
+        return response.json()
+    })
+    .then(function (data){
+        console.log(data)
+    })
+    .catch(function (error) {
+        alert('Unauthorized Access')
+        return console.log(error.message)
+    })
+}
+*/
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+    .then(function (response){
+        return response.json()
+    })
+    .then(function (data){
+        console.log(data)
+        document.body.innerHTML = data.id
+    })
+    .catch(function (error) {
+        alert('Unauthorized Access')
+        console.log(error['message'])
+        document.body.innerHTML = error.message
+    })
+}
